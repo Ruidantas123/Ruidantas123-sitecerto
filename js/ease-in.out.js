cc7a5a656b9f95e9b11f3b63f6a7336201c82913
Rui Dantas <ruidantas25@gmail.com>
2023-05-19 10:51:23 +0100

    avav

--- js/ease-in.out.js
+++ js/ease-in.out.js
@@ -1,15 +1,25 @@
 window.addEventListener('scroll', function() {
-    var menu = document.getElementById('menu');
-    var menuOffset = menu.offsetTop;
-    var scrollPosition = window.scrollY;
+  var menu = document.getElementById('menu');
+  var menuOffset = menu.offsetTop;
+  var scrollPosition = window.scrollY;
+  var windowHeight = window.innerHeight;
+  var body = document.body;
+  var originalBackgroundColor = window.getComputedStyle(body).getPropertyValue('background-color');
+
+
   
-    if (scrollPosition > menuOffset) {
-      menu.classList.remove('hidden');
-      menu.style.backgroundColor = '#4f9cf9';
-      menu.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.5)';
-    } else {
-        
-      menu.style.backgroundColor = ''; // Retorna ao estilo original quando o menu não está visível
-      menu.style.boxShadow = 'none';
-    }
-  });
+  if (scrollPosition === 0) {
+    menu.classList.remove('ease-in-out');
+    menu.style.backgroundColor = '';
+    menu.style.boxShadow = 'none';
+  } else if (scrollPosition > menuOffset - windowHeight / 2) {
+    menu.classList.remove('hidden');
+    menu.classList.add('ease-in-out');
+    menu.style.backgroundColor = ('#4f9cf9');
+    menu.style.boxShadow = '0px 1px 2px rgba(0, 0, 0, 0.5)';
+  } else {
+    menu.classList.remove('ease-in-out');
+    menu.style.backgroundColor = '';
+    menu.style.boxShadow = 'none';
+  }
+});
