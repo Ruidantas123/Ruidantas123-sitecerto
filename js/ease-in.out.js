19461fe5609f92cf93668bddb3d3caabe3806d2e
Rui Dantas <ruidantas25@gmail.com>
2023-05-26 09:02:08 +0100

    fasf

--- js/ease-in.out.js
+++ js/ease-in.out.js
@@ -5,15 +5,19 @@ window.addEventListener('scroll', function() {
   var windowHeight = window.innerHeight;
   var body = document.body;
 
+  var isMenuOpen = menu.classList.contains('open');
 
-  
-  if (scrollPosition === 0) {
-    menu.classList.remove('ease-in-out');
+  if (scrollPosition === 0 || isMenuOpen) {
+    menu.style.transition = 'none';
     menu.style.backgroundColor = '';
     menu.style.boxShadow = 'none';
   } else if (scrollPosition > menuOffset - windowHeight / 2) {
-    menu.classList.add('ease-in-out');
-    menu.style.backgroundColor = ('#4f9cf9');
+    menu.style.transition = 'background-color 0.5s ease-in-out, box-shadow 0.5s ease-in-out';
+    menu.style.backgroundColor = '#249c88';
     menu.style.boxShadow = '0px 1px 2px rgba(0, 0, 0, 0.5)';
+  } else {
+    menu.style.transition = 'background-color 0.5s ease-in-out, box-shadow 0.5s ease-in-out';
+    menu.style.backgroundColor = '';
+    menu.style.boxShadow = 'none';
   }
 });
