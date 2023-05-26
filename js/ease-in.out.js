206b91c442ef500ee7e8ef17cefac96d2900f5a0
Rui Dantas <ruidantas25@gmail.com>
2023-05-26 09:01:52 +0100

    asds

--- js/ease-in.out.js
+++ js/ease-in.out.js
@@ -4,7 +4,6 @@ window.addEventListener('scroll', function() {
   var scrollPosition = window.scrollY;
   var windowHeight = window.innerHeight;
   var body = document.body;
-  var originalBackgroundColor = window.getComputedStyle(body).getPropertyValue('background-color');
 
 
   
@@ -13,13 +12,8 @@ window.addEventListener('scroll', function() {
     menu.style.backgroundColor = '';
     menu.style.boxShadow = 'none';
   } else if (scrollPosition > menuOffset - windowHeight / 2) {
-    menu.classList.remove('hidden');
     menu.classList.add('ease-in-out');
     menu.style.backgroundColor = ('#4f9cf9');
     menu.style.boxShadow = '0px 1px 2px rgba(0, 0, 0, 0.5)';
-  } else {
-    menu.classList.remove('ease-in-out');
-    menu.style.backgroundColor = '';
-    menu.style.boxShadow = 'none';
   }
 });
