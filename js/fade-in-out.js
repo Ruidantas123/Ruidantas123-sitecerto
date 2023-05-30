f649dd647c973ae7d212e8d7dc144a1726f119dd
Rui Dantas <ruidantas25@gmail.com>
2023-05-30 09:22:31 +0100

    sfsd

--- js/fade-in-out.js
+++ js/fade-in-out.js
@@ -1,54 +1,63 @@
 window.addEventListener('scroll', function() {
-    var empresa = document.querySelector('.empresa');
-    var h2Empresa = empresa.querySelector('h2');
-  
-    var containerEmpresa = document.querySelector('.container-empresa');
-    var links = containerEmpresa.querySelectorAll('a');
-  
-    var facaMaisText = document.querySelector('.faca-mais-text');
-    var h3FacaMais = facaMaisText.querySelector('h3');
-    var pFacaMais = facaMaisText.querySelector('p');
+  var empresa = document.querySelector('.empresa');
+  var h2Empresa = empresa.querySelector('h2');
 
-    var exemplo = document.querySelector('.exemplo  ')
-  
-    if (isElementPartiallyInViewport(empresa)) {
+  var containerEmpresa = document.querySelector('.container-empresa');
+  var links = containerEmpresa.querySelectorAll('a');
+
+  var facaMaisText = document.querySelector('.faca-mais-text');
+  var h3FacaMais = facaMaisText.querySelector('h3');
+  var pFacaMais = facaMaisText.querySelector('p');
+
+  var bannerText = document.querySelector('.banner-text');
+  var h1bannerText = bannerText.querySelector('h1');
+  var btnBanner = bannerText.querySelector('.btn');
+
+  if (isElementPartiallyInViewport(empresa)) {
       fadeInOut(h2Empresa);
-    }
-  
-    links.forEach(function(link) {
+  }
+
+  links.forEach(function(link) {
       if (isElementPartiallyInViewport(containerEmpresa)) {
-        fadeInOut(link);
+          fadeInOut(link);
       }
-    });
-  
-    if (isElementPartiallyInViewport(facaMaisText)) {
-      fadeInOut(h3FacaMais);
-      fadeInOut(pFacaMais);
-    }
   });
 
-  if(isElementPartiallyInViewport(exemplo)) {
-    fadeInOut(exemplo);
+  if (isElementPartiallyInViewport(facaMaisText)) {
+      fadeInOut(h3FacaMais);
+      fadeInOut(pFacaMais);
   }
-  
-  function isElementPartiallyInViewport(element) {
-    var rect = element.getBoundingClientRect();
-    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
-    return (
+
+  if (isElementPartiallyInViewport(bannerText)) {
+      fadeInOut(h1bannerText);
+      fadeInOut(btnBanner);
+  }
+});
+
+
+function isElementPartiallyInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  return (
       rect.bottom >= 0 &&
       rect.top <= windowHeight
-    );
+  );
+}
+
+function fadeInOut(element) {
+  var fadeEffect;
+  var currentOpacity = parseFloat(element.style.opacity) || 0;
+  if (currentOpacity < 1) {
+      fadeEffect = setInterval(function() {
+          if (!element.style.opacity) {
+              element.style.opacity = 0;
+          }
+          if (element.style.opacity < 1) {
+              currentOpacity += 0.05; // Ajuste a velocidade da animação modificando o valor 0.05
+              element.style.opacity = currentOpacity;
+          } else {
+              clearInterval(fadeEffect);
+          }
+      }, 50); // Ajuste a velocidade da animação modificando o valor 50
   }
-  
-  function fadeInOut(element) {
-    var fadeEffect = setInterval(function() {
-      if (!element.style.opacity) {
-        element.style.opacity = 0;
-      }
-      if (element.style.opacity < 1) {
-        element.style.opacity = parseFloat(element.style.opacity) + 0.05; // Ajuste a velocidade da animação modificando o valor 0.05
-      } else {
-        clearInterval(fadeEffect);
-      }
-    }, 50); // Ajuste a velocidade da animação modificando o valor 50
-  }
+}
